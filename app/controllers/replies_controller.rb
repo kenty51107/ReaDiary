1b5f6f40b10ab31d2f459739a5f3c01bf5cf0d98
kenty51107 <iken51107@gmail.com>
2023-04-06 18:50:57 +0900

    [add] 選択中のメニューボタンに装飾を追加

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,7 +1,7 @@
 class RepliesController < ApplicationController
   def create
     @reply = Reply.new(reply_params)
-    if @reply.save!
+    if @reply.save
       redirect_to record_path(@reply.record_id), notice: "コメントを投稿しました。"
     else
       flash.notice = "コメントを投稿できませんでした。"
