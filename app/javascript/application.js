43f676302db63be80a4db91377bbcd31be82d12f
kenty51107 <iken51107@gmail.com>
2023-04-09 03:27:23 +0900

    [fix] ページ更新時にちらつく本棚のタブを修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 Turbo.session.drive = false
+
+// ページ更新時、本棚のタブがちらつくのを防ぐための処理
+let el = document.querySelector("#initial");
+setTimeout(() => {
+  el.id = "";
+},100);
