e627457111f772ae37b2eeec13f5e1dd75d87576
kenty51107 <iken51107@gmail.com>
2023-04-14 19:28:34 +0900

    [update] アカウント情報更新時のパスワード要求を省略

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,17 +38,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  # アカウント情報変更時にパスワード入力を省略
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
+  # update後にユーザーページにれダイレクト
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,20 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  # フォローしているユーザーと自分の投稿を取得する
+
+  # アカウント情報変更時にパスワード入力を省略
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
