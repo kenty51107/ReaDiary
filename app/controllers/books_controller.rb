8ef688739849480be313d548a294b8001598bd00
kenty51107 <iken51107@gmail.com>
2023-04-23 18:51:44 +0900

    [update] 詳しく見るをshowからdetailアクションに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def show
-    @book = current_user.books.find(params[:id])
-  end
-
   def edit
     @book = current_user.books.find(params[:id])
   end
